b836d02011449c8959353874935f6e787841d55c
kameleon <kam.global@pm.me>
2023-08-15 17:48:20 +0200

    Fix typo

--- .space.kts
+++ .space.kts
@@ -1,20 +1,21 @@
 job("Build and push Docker") {
     host("Build and push the Docker image") {
-      shellScript {
-        content = """
-            # Install latest docker (https://www.docker.com/blog/multi-arch-build-what-about-travis)
-            sudo rm -rf /var/lib/apt/lists/*
-            curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-            sudo add-apt-repository \"deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) edge\"
-            sudo apt-get update
-            sudo apt-get -y -o Dpkg::Options::=\"--force-confnew\" install docker-ce
-            mkdir -vp ~/.docker/cli-plugins/
-            curl --silent -L \"https://github.com/docker/buildx/releases/download/v0.11.2/buildx-v0.11.2.linux-arm64\" > ~/.docker/cli-plugins/docker-buildx
-            chmod a+x ~/.docker/cli-plugins/docker-buildx
-            docker buildx create --use
-            docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-            docker buildx inspect --bootstrap
-        """
+        shellScript {
+            content = """
+                # Install latest docker (https://www.docker.com/blog/multi-arch-build-what-about-travis)
+                sudo rm -rf /var/lib/apt/lists/*
+                curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+                sudo add-apt-repository \"deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) edge\"
+                sudo apt-get update
+                sudo apt-get -y -o Dpkg::Options::=\"--force-confnew\" install docker-ce
+                mkdir -vp ~/.docker/cli-plugins/
+                curl --silent -L \"https://github.com/docker/buildx/releases/download/v0.11.2/buildx-v0.11.2.linux-arm64\" > ~/.docker/cli-plugins/docker-buildx
+                chmod a+x ~/.docker/cli-plugins/docker-buildx
+                docker buildx create --use
+                docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+                docker buildx inspect --bootstrap
+            """
+        }
         dockerBuildPush {
             // Docker context, by default, project root
             // context = "docker"
